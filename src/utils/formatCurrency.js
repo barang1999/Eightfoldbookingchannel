62cc60f8a16db776207b4d38993a4e28feb31855
barang1999 <barang.business@gmail.com>
2025-06-18 13:40:04 +0700

    update

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,10 +1,10 @@
 // src/utils/formatCurrency.js
 export function formatCurrency(amountUSD, exchangeRate, currencyCode) {
   const converted = amountUSD * exchangeRate;
-
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currencyCode || 'USD',
+  const number = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
     maximumFractionDigits: 2
   }).format(converted);
+
+  return `${currencyCode || 'USD'} ${number}`;
 }
